16b8bad24883c63c1b1112824c47013a4255789c
Serj Kalichev <serj.kalichev@gmail.com>
2025-07-11 12:32:16 +0300

    plugin_klish: Plugin uses new ksym_new_fast() function.

--- plugins/klish/plugin_init.c
+++ plugins/klish/plugin_init.c
@@ -26,20 +26,14 @@ int kplugin_klish_init(kcontext_t *context)
 	assert(plugin);
 
 	// Misc
-	kplugin_add_syms(plugin, ksym_new_ext("nop", klish_nop,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("tsym", klish_tsym,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("print", klish_print,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("printl", klish_printl,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("prompt", klish_prompt,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
+	kplugin_add_syms(plugin, ksym_new_fast("nop", klish_nop));
+	kplugin_add_syms(plugin, ksym_new_fast("tsym", klish_tsym));
+	kplugin_add_syms(plugin, ksym_new_fast("print", klish_print));
+	kplugin_add_syms(plugin, ksym_new_fast("printl", klish_printl));
+	kplugin_add_syms(plugin, ksym_new_fast("prompt", klish_prompt));
 
 	// Log
-	kplugin_add_syms(plugin, ksym_new_ext("syslog", klish_syslog,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
+	kplugin_add_syms(plugin, ksym_new_fast("syslog", klish_syslog));
 
 	// Navigation
 	// Navigation must be permanent (no dry-run) and sync. Because unsync
@@ -51,22 +45,16 @@ int kplugin_klish_init(kcontext_t *context)
 
 	// PTYPEs
 	// These PTYPEs are simple and fast so set SYNC flag
-	kplugin_add_syms(plugin, ksym_new_ext("COMMAND", klish_ptype_COMMAND,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("completion_COMMAND",
-		klish_completion_COMMAND,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("help_COMMAND", klish_help_COMMAND,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("COMMAND_CASE",
-		klish_ptype_COMMAND_CASE,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("INT", klish_ptype_INT,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("UINT", klish_ptype_UINT,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
-	kplugin_add_syms(plugin, ksym_new_ext("STRING", klish_ptype_STRING,
-		KSYM_USERDEFINED_PERMANENT, KSYM_SYNC, KSYM_SILENT));
+	kplugin_add_syms(plugin, ksym_new_fast("COMMAND", klish_ptype_COMMAND));
+	kplugin_add_syms(plugin, ksym_new_fast("completion_COMMAND",
+		klish_completion_COMMAND));
+	kplugin_add_syms(plugin, ksym_new_fast("help_COMMAND",
+		klish_help_COMMAND));
+	kplugin_add_syms(plugin, ksym_new_fast("COMMAND_CASE",
+		klish_ptype_COMMAND_CASE));
+	kplugin_add_syms(plugin, ksym_new_fast("INT", klish_ptype_INT));
+	kplugin_add_syms(plugin, ksym_new_fast("UINT", klish_ptype_UINT));
+	kplugin_add_syms(plugin, ksym_new_fast("STRING", klish_ptype_STRING));
 
 	return 0;
 }
